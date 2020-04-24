5a5618c2ee1ead48be9162f30c68002625546b2b
Owen Bezick <owbezick@davidson.edu>
2020-04-24 10:22:57 -0400

    key terms sentiment

--- interviewTextScraping/dataIntake.R
+++ interviewTextScraping/dataIntake.R
@@ -9,7 +9,7 @@ library(rlist)
 library(tokenizers)
 library(stopwords)
 library(syuzhet)
-
+library(echarts4r)
 # Import Leader pdf file ----
 raw_lines <- pdf_text("leader_interview.pdf") %>%
   read_lines()
@@ -61,7 +61,7 @@ q_term_freq[1:100]
 # Sentiment Analysis ----
 # Create dataframe
 answers_df <- data.frame(text=unlist(sapply(clean_answers, `[`, "content")), 
-                        stringsAsFactors=F)
+                         stringsAsFactors=F)
 
 # Get lists of questions
 answers_ls <- answers_df %>%
@@ -70,6 +70,63 @@ answers_ls <- answers_df %>%
 
 # Sentiment for each question
 answer_sentiment <- get_nrc_sentiment(answers_ls)
+
 # Add answer back into sentiment
 answer_sentiment$answer = answers_ls
 
+# Overall Sentiment Bar Chart 
+sentimentBar <- function(df, title){
+  df %>%
+    e_chart(chart) %>%
+    e_bar("negative", name = "Negative", color = "#543005") %>%
+    e_bar("anger", name = "Anger", color = "#8c510a") %>%
+    e_bar("disgust", name = "Disgust", color = "#bf812d") %>%
+    e_bar("fear", name = "Fear", color = "#dfc27d") %>%
+    e_bar("sadness", name = "Sadness", color = "#f6e8c3") %>%
+    e_bar("anticipation", name= "Anticipation", color = "#c7eae5") %>%
+    e_bar("joy", name = "Joy", color = "#80cdc1") %>%
+    e_bar("surprise", name = "Surprise", color = "#35978f") %>%
+    e_bar("trust", name = "Trust", color = "#01665e") %>%
+    e_bar("positive", name = "Positive", color = "#003c30") %>%
+    e_axis_labels(x = "Emotion", y = "Number of Tags") %>%
+    e_title(title) %>%
+    e_legend(bottom = 0) %>%
+    e_tooltip()
+}
+
+# Data
+overall_sentiment <- answer_sentiment %>%
+  summarise(anger = sum(anger)
+            , disgust = sum(disgust)
+            , fear = sum(fear)
+            , joy = sum(joy)
+            , sadness = sum(sadness)
+            , surprise = sum(surprise)
+            , trust = sum(trust)
+            , anticipation = sum(anticipation)
+            , negative = sum(negative)
+            , positive = sum(positive)) %>%
+  mutate(chart = "")
+
+overall_sentiment_bar <- sentimentBar(overall_sentiment, "Overall Interview Sentiment")
+
+# Key Word Sentiment
+# Find answers with keywords
+key_words_df <-answer_sentiment %>%
+  mutate(rwanda = ifelse(str_detect(answer, "rwanda"), 1, 0)) %>%
+  mutate(genocide = ifelse(str_detect(answer, "genocide"), 1, 0)) %>%
+  mutate(rwandan = ifelse(str_detect(answer, "rwandan"), 1, 0)) %>%
+  filter(rwanda > 0 | genocide >0 | rwandan >0) %>%
+  summarise(anger = sum(anger)
+            , disgust = sum(disgust)
+            , fear = sum(fear)
+            , joy = sum(joy)
+            , sadness = sum(sadness)
+            , surprise = sum(surprise)
+            , trust = sum(trust)
+            , anticipation = sum(anticipation)
+            , negative = sum(negative)
+            , positive = sum(positive)) %>%
+  mutate(chart = "")
+
+key_word_sentiment_bar <- sentimentBar(key_words_df, "Answers Mentioning Rwanda & Genocide")
